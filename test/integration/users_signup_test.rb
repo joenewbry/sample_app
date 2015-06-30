e4cc01ff1d8a888582986bacc0aa70e510cdf78b
Joe Newbry <joenewbry@gmail.com>
2015-06-30 14:31:04 -0700

    Chapter 10 is right

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -29,7 +29,17 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     assert_equal 1, ActionMailer::Base.deliveries.size
     user = assigns(:user)
-    # valid activation token
+    assert_not user.activated?
+    # tryto log in before activation
+    log_in_as(user)
+    assert_not is_logged_in?
+    # INvalid activation token
+    get edit_account_activation_path("invalid token")
+    assert_not is_logged_in?
+    # Valid token, wrong email
+    get edit_account_activation_path(user.activation_token, email: 'wrong')
+    assert_not is_logged_in?
+    # Valid activation token
     get edit_account_activation_path(user.activation_token, email: user.email)
     assert user.reload.activated?
     follow_redirect!
