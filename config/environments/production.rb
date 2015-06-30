0d45e9fee88c533952bf4e3e1a59c1d9854ccdb9
Joe Newbry <joenewbry@gmail.com>
2015-06-30 15:23:55 -0700

    Update files for prod environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   host = 'peaceful-inlet-7329.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smtp_settings = {
+ eectionMailer::Base.smtp_settings = {
     :address          => 'smtp.sendgrid.net',
     :port             => '587',
     :authentication   => :plain,
