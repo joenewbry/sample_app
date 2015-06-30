879122ff00eb0f9876dfee5bcda376bbcf55655c
Joe Newbry <joenewbry@gmail.com>
2015-06-30 15:04:45 -0700

    Fix spelling error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = 'peaceful-inlet-7329.herokuapp.com'
-  config.action_mailer.default_url_optinos = { host: host }
+  config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address          => 'smtp.sendgrid.net',
     :port             => '587',
