e6301de416821c36297fa5a068d4d7295e0b219e
Joe Newbry <joenewbry@gmail.com>
2015-06-30 15:54:05 -0700

    setup amazon access keys

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,7 +4,7 @@ if Rails.env.production?
     # Amazon S3 configuration
     :provider             => 'AWS',
     :aws_access_key_id    => ENV['S3_ACCESS_KEY'],
-    :asw_secret_access_key => ENV['S3_SECRET_KEY']
+    :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
     config.fog_directory = ENV['S3_BUCKET']
   end
